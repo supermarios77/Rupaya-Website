f427fbcc9b61182ea207410ebb1e8307f9a92f11
SuperMarios <110827896+supermariya@users.noreply.github.com>
2023-08-05 13:50:56 +0300

    Add Home Page Styling And Code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,15 +16,67 @@ function Home() {
 
       <div className="home-text">
         <p>
-          At Rupaya, {" "}
-          <a className="Highlighted-Text" href="https://bitcointalk.org/index.php?topic=521251.0">
+          At Rupaya,{" "}
+          <a
+            className="Highlighted-Text"
+            href="https://bitcointalk.org/index.php?topic=521251.0"
+          >
             we embarked on a journey in 2014
-          </a> {" "}
+          </a>{" "}
           with a mission to bring awareness of cryptocurrencies and financial
-          freedom to the people of South Asia. <br /> 
-          Over the years, our project has
-          evolved, adapting to the ever-changing landscape of the crypto market.
+          freedom to the people of South Asia. <br />
+          Over the years, our project has evolved, adapting to the ever-changing
+          landscape of the crypto market.
         </p>
+      </div>
+
+      <div className="row">
+        <div className="Short-Overview">
+          <i class="fa-regular fa-clock"></i>
+          <p>
+            Our story began with a pioneering mission to introduce
+            cryptocurrencies to South Asia. Since then, our community has grown,
+            embracing the power of decentralised finance.
+          </p>
+        </div>
+        <div className="Short-Overview">
+          <i class="fa-solid fa-road"></i>
+          <p>
+            Our journey has been marked by challenges, but with each obstacle,
+            we have grown stronger, always committed to our mission
+          </p>
+        </div>
+        <div className="Short-Overview">
+          <i class="fa-solid fa-crown"></i>
+          <p>
+            From humble beginnings as a scrypt PoW blockchain, we have embraced
+            innovation to become an ERC20 token on Binance Chain, empowering
+            global accessibility
+          </p>
+        </div>
+        <div className="Short-Overview">
+          <i class="fa-solid fa-mountain"></i>
+          <p>
+            Looking ahead, we envision revitalizing our project by assembling a
+            skilled team and forging strategic partnerships to unlock new
+            possibilities.
+          </p>
+        </div>
+        <div className="Short-Overview">
+          <i class="fa-solid fa-globe"></i>
+          <p>
+            While our roots are in South Asia, our vision now extends worldwide.
+            We aim to foster financial inclusion across borders and cultures.
+          </p>
+        </div>
+        <div className="Short-Overview">
+          <i class="fa-solid fa-thumbs-up"></i>
+          <p>
+            We invite you to join us as we explore the boundless potential of
+            blockchain technology to create a more equitable and inclusive
+            financial landscape for all.
+          </p>
+        </div>
       </div>
     </div>
   );

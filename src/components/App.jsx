8d23fb0b5e2bf1f0aa4315c2ae81bfda889a37a8
SuperMarios <110827896+supermariya@users.noreply.github.com>
2023-08-06 17:44:48 +0300

    Add Community Page Styling And Code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Contact from "./Contact";
+import Community from "./Community";
 
 
 
@@ -9,7 +9,7 @@ function App() {
     return (
         <div>
             <Header />
-            <Contact />
+            <Community />
             <Footer />
         </div>
     );
--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -3,7 +3,135 @@ import React from "react";
 function Community() {
   return (
     <div className="Community">
-        
+      <div className="Community-Heading">
+        <h1>Join Our Community!</h1>
+        <h3>
+          Welcome to the heart of Rupaya's journey - our vibrant and inclusive
+          community! We believe that collaboration and shared values drive the
+          path to true financial freedom. Together, we are empowering
+          individuals worldwide through blockchain technology.
+        </h3>
+      </div>
+
+      <div className="Community-Text-Box">
+        <h2>Stay Connected on Social Media</h2>
+        <p>
+          Stay up-to-date with the latest news, updates, and announcements by
+          following our official social media channels. Join the conversations,
+          share your insights, and be part of the growing Rupaya family:{"  "}
+          <a href="https://discord.com/invite/8tbvMQM">
+            <i class="fa-brands fa-discord"></i>
+          </a>
+          <a href="https://www.facebook.com/rupayacoin">
+            <i class="fa-brands fa-facebook"></i>
+          </a>
+          <a href="https://twitter.com/RupayaCoin">
+            <i class="fa-brands fa-twitter"></i>
+          </a>
+          <a href="https://www.reddit.com/r/RupayaCoin/">
+            <i class="fa-brands fa-reddit"></i>
+          </a>
+        </p>
+        <span className="semiCircle">
+          <i class="fa-solid fa-comments"></i>
+        </span>
+      </div>
+
+      <div className="Community-Text-Box">
+        <h2>Engaging Community Events</h2>
+        <p>
+          We host a range of exciting community events, including AMAs (Ask Me
+          Anything), webinars, and meetups. Participate in these gatherings to
+          interact directly with our team and fellow community members. It's an
+          opportunity to share ideas, ask questions, and celebrate achievements
+          together.
+        </p>
+        <span className="semiCircle">
+          <i class="fa-solid fa-user-group"></i>
+        </span>
+      </div>
+
+      <div className="Community-Text-Box">
+        <h2>Community Guidelines</h2>
+        <p>
+          We foster a positive and supportive environment for all community
+          members. To maintain a healthy community culture, we kindly ask
+          everyone to adhere to our community guidelines:Be respectful and
+          considerate of others' opinions.Avoid spamming or spreading
+          misinformation.Stay on-topic and relevant to the community's
+          interests.Refrain from engaging in any form of harassment or hate
+          speech.Failure to comply with these guidelines may result in
+          moderation actions or removal from the community.
+        </p>
+        <span className="semiCircle">
+          <i class="fa-solid fa-handshake"></i>
+        </span>
+      </div>
+
+      <div className="Community-Text-Box">
+        <h2>Ambassadors and Contributors</h2>
+        <p>
+          Our community is powered by dedicated ambassadors and contributors who
+          play a crucial role in the growth of Rupaya. These outstanding
+          individuals actively engage in promoting our project, organizing
+          events, and assisting new members.
+        </p>
+        <span className="semiCircle">
+          <i class="fa-solid fa-user"></i>
+        </span>
+      </div>
+
+      <div className="Community-Text-Box">
+        <h2>Share Your Feedback</h2>
+        <p>
+          We value your feedback, suggestions, and ideas to make Rupaya even
+          better. Whether you have technical insights or suggestions for
+          community engagement, we'd love to hear from you. Feel free to share
+          your thoughts through our contact form.
+        </p>
+        <span className="semiCircle">
+          <i class="fa-solid fa-message"></i>
+        </span>
+      </div>
+
+      <div className="Community-Text-Box">
+        <h2>Volunteer Opportunities</h2>
+        <p>
+          If you are passionate about blockchain technology and believe in the
+          mission of Rupaya, there are volunteer opportunities available. Join
+          us in shaping the future of financial freedom and contributing your
+          skills to our community-driven project.
+        </p>
+        <span className="semiCircle">
+          <i class="fa-solid fa-add"></i>
+        </span>
+      </div>
+
+      <div className="Community-Text-Box">
+        <h2>Join the Discussion</h2>
+        <p>
+          We encourage you to actively participate in discussions within our
+          community. Engage in open conversations, ask questions, and share your
+          experiences with other community members. Together, we can drive
+          meaningful change and inspire others to embrace blockchain technology.
+        </p>
+        <span className="semiCircle">
+          <i class="fa-solid fa-walkie-talkie"></i>
+        </span>
+      </div>
+
+      <div className="Community-Text-Box">
+        <h2>Newsletter Subscription</h2>
+        <p>
+          Stay informed about our latest updates, project developments, and
+          important announcements by subscribing to our newsletter. Don't miss
+          out on exciting opportunities and insightful content delivered
+          directly to your inbox.
+        </p>
+        <span className="semiCircle">
+          <i class="fa-solid fa-envelope"></i>
+        </span>
+      </div>
     </div>
   );
 }
